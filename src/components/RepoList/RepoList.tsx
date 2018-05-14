c19f1dc6dbf29fe25a3b6bd85dba5849ceffe72f
lucasfrosty <lucas_frosty@hotmail.com>
2018-05-14 07:04:13 -0300

    repos placeholder styling setup

--- src/components/RepoList/RepoList.tsx
+++ src/components/RepoList/RepoList.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
+import './RepoList.css';
 
 interface Props {
   language: string | undefined;
 }
 
 function RepoList({ language }: Props) {
-  return <p>{language || 'Loading...'}</p>
+  return (
+    <React.Fragment>
+      <h1>{language}</h1>
+
+      <div>
+        <div className="flex-container">
+          <span className="bold">Stars:</span> 1302
+        </div>
+        <div className="flex-container">
+          <span className="bold">Owner:</span>
+          <img
+            src='https://avatars2.githubusercontent.com/u/15235605?s=400&u=5f1a369caf235bff10d6d4cf0351506b7b317e45&v=4'
+            className="logo"
+            alt="Owner Logo"
+          />
+          Lucas Ferreira
+        </div>
+        <div className="flex-container">
+          <span className="bold">Repo:</span>{" "}
+          <a target="_blank" href='#'>
+            idk
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default RepoList;
